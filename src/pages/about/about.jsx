8621c412879ab300185507a2b212dd7ca9b61da6
Ferry L.H <ferrylinton@gmail.com>
2021-12-02 12:03:25 +0700

    update author page and about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -5,7 +5,15 @@ function About() {
     <div className="d-flex justify-content-center">
       <div className="about-box">
         <div className="about-title">todo <span>app</span></div>
-        <div className="about-description">A simple application build with ReactJS with no access to backend</div>
+        <div className="about-description">A simple application build with ReactJS <br/> with no access to backend</div>
+        <div className="libraries">
+          <h2>Libraries</h2>
+          <h5>React</h5>
+          <h5>React Bootstrap & Bootsrap</h5>
+          <h5>React Hook Form & Yup</h5>
+          <h5>React Icons</h5>
+          <h5>Moment</h5>
+        </div>
       </div>
     </div>
   )
--- src/pages/author/author.jsx
+++ src/pages/author/author.jsx
@@ -1,14 +1,40 @@
 import "./author.scss";
 import AuthorPng from '../../images/author.png';
 
-function Author(){
+function Author() {
 
-  return(
+  return (
     <div className="d-flex justify-content-center">
       <div className="author-box">
-        <img src={AuthorPng} alt="Author" className="author-img"  />
+        <img src={AuthorPng} alt="Author" className="author-img" />
         <div className="author-name">Ferry L. H.</div>
         <div className="author-title">I am a Software Developer</div>
+        <div className="author-email">ferrylinton@gmail.com</div>
+
+        <div className="author-description">
+          <p>
+            I have participated in several projects that create applications based on the Java programming language.
+            Here are some of the frameworks I have used:
+          </p>
+          <ul>
+            <li>
+              <strong>Java</strong><br/>
+              <span>
+                Springboot, Spring MVC, Spring Data JPA, Hibernate, Spring Security, Spring OAuth2, MITREid Connect, Apache Camel, Apache CXF)
+              </span>
+            </li>
+            <li>
+              <strong>Javascript</strong><br/>
+              <span>Angular, Jquery</span>
+            </li>
+            <li><strong>Bootstrap</strong></li>
+            <li>
+              <strong>Database</strong><br/>
+              <span>Postgresql, Oracle, Mysql</span>
+            </li>
+          </ul>
+
+        </div>
       </div>
     </div>
   )
